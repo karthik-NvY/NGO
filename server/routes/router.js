dab093023e04e860ae0aafd2392e49c8213d7fd0
Div16s <divyankarshah1602@gmail.com>
2024-02-08 20:06:07 +0530

    Added endpoint for signup

---
+++ server/routes/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = new express.Router();
+const { userSignup, sendOTP_userSignup } = require('./api/userSignup');
+
+//Routes
+router.post("/user/signup", userSignup);
+router.post("/user/signup/authenticateOTP",sendOTP_userSignup)
+
+module.exports = router;
