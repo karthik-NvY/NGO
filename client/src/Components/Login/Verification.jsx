1f5cdd3f2a7ca8a30a6285489c54e8197f83fc2e
Doodh Nath Tiwari <doodhnathtiwari7@gmail.com>
2024-02-10 21:13:59 +0530

    OTP Verification Page completed

--- client/src/Components/Login/Verification.jsx
+++ client/src/Components/Login/Verification.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import "./Verification.css";
 
 import causecraft_logo from "../Assets/causecraft_logo.png";
 
-
-
 function Verification() {
   return (
     <div>
-        <div className="container">
-            <h1>Verification</h1>
-            <h2>Please Enter the OTP to verify your account</h2>
+      <img src={causecraft_logo} alt="background img" />
+      <div className="container">
+        <h1>Verification</h1>
+        <h2>Please Enter the OTP to verify your account</h2>
+        <div className="input">
+          <input type="text" />
         </div>
-        <div className="otp_container">
-            <div className="otp_box">
-                
-            </div>
+        <div className="submit-container">
+          {/* Buttons to toggle between Register and Login */}
+          <div className="submit">
+            <span>Verify OTP</span>
+          </div>
+
+          <div className="resend-otp">
+            Didn't get the code?{" "}
+            <span>
+              <u>Resend</u>
+            </span>
+          </div>
         </div>
-   
-
-
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Verification
-
-
+export default Verification;
--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import { LoginSignup } from './Components/Login/LoginSignup';
 import reportWebVitals from './reportWebVitals';
 import Login from './Components/Login/Login';
+import Verification  from './Components/Login/Verification'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginSignup />
+    {/* <LoginSignup /> */}
     {/* <Login /> */}
+    <Verification />
   </React.StrictMode>
 );
 
