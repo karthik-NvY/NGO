ded3ca939872f9f1d44e1b63cef83eafaed7d36a
Doodh Nath Tiwari <doodhnathtiwari7@gmail.com>
2024-02-10 20:23:56 +0530

    Added UI/UX design to the login signup page

---
+++ client/src/Components/Login/Verification.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import React, { useState } from "react";
+import "./Verification.css";
+
+import causecraft_logo from "../Assets/causecraft_logo.png";
+
+
+
+function Verification() {
+  return (
+    <div>
+        <div className="container">
+            <h1>Verification</h1>
+            <h2>Please Enter the OTP to verify your account</h2>
+        </div>
+        <div className="otp_container">
+            <div className="otp_box">
+                
+            </div>
+        </div>
+   
+
+
+
+    </div>
+  )
+}
+
+export default Verification
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { LoginSignup } from './Components/Login/LoginSignup';
 import reportWebVitals from './reportWebVitals';
+import Login from './Components/Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <LoginSignup />
+    {/* <Login /> */}
   </React.StrictMode>
 );
 
