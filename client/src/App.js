76e37c033455e57bd7a88e48dc0aa4b187aef5ee
Nitesh kumar <niteshgurjar2612@gmail.com>
2024-02-19 02:05:06 +0530

    page update 1

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { Homepage } from './Components/Pages/Homepage/Homepage';
 import { LoginSignup } from './Components/Pages/Login/LoginSignup';
 import { Profilepage } from './Components/Pages/Profilepage/Profilepage';
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
    <>
     {/* <Homepage/>  */}
-    <LoginSignup/>
-    {/* <Profilepage/> */}
+    {/* <LoginSignup/> */}
+    <Profilepage/>
    
    </>
   );
--- client/src/Components/Pages/Profilepage/Profilepage.jsx
+++ client/src/Components/Pages/Profilepage/Profilepage.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+// Profilepage.js
 
+import React from 'react';
+import './Profilepage.css';
+import causecraft_logo from '../../Assets/causecraft_logo.png';
+import profile_icon from '../../Assets/user-icon.webp';
+import home_icon from '../../Assets/home-icon.png'; 
+
+const UserName = 'Username';
 export const Profilepage = () => {
   return (
-    <div>Profilepage</div>
-  )
-}
+    <div className='body'>
+      <div className="container">
+        <div className="header-container">
+          <div className="header-logo">
+            <img src={causecraft_logo} alt="Causecraft Logo" />
+          </div>
+          <div className="header-home">
+            <button>
+              <img src={home_icon} alt="Home Icon" />
+              Home
+            </button>
+          </div>
+        </div>
+        
+        
+
+        
+        <div className="middle-container">
+          <p>{UserName}</p>
+        </div>
+
+      
+
+        <div className="footer-container">
+          <div className="footer-button">As a volunteer in NGOs</div>
+          <div className="footer-button">As a donor in NGOs</div>
+          <div className="footer-button">As an executive in NGOs</div>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
