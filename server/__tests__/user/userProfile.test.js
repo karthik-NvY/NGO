3946521c48988fe73baeffc30edb2a19334841a3
karthik-NvY <karthikndasaraju@gmail.com>
2024-03-19 15:57:12 +0530

    updates on profile testing

--- server/__tests__/user/userProfile.test.js
+++ server/__tests__/user/userProfile.test.js
@@ -1,12 +1,33 @@
 const request = require('supertest')
 
+const UsersAuthController = require('../../controllers/userAuthController');
+const Users = require('../../models/userModel');
+
 // Required.
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2FpIERhdHRhIiwiZW1haWwiOiIyMDIxY3NiMTEwNkBpaXRycHIuYWMuaW4iLCJpZCI6IjY1ZGExMTdjMjIxNjExMWJmZjVkMGJhOCIsImlhdCI6MTcxMDc5NjY0MiwiZXhwIjoxNzEwODgzMDQyfQ.NO5ON5RKb23gw3uiJHT61jsArdSfgyILaxFv8VRMEL8';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2FpIERhdHRhIiwiZW1haWwiOiIyMDIxY3NiMTEwNkBpaXRycHIuYWMuaW4iLCJpZCI6IjY1ZGExMTdjMjIxNjExMWJmZjVkMGJhOCIsImlhdCI6MTcxMDg0MzQyOCwiZXhwIjoxNzEwOTI5ODI4fQ.RQEwM0GL9tbiecjaqSU0iS27cUtuAgZ0SnBgjo75JCA';
 
 describe('User Profile', () => {
     test('Successful in fetchin user profile', async () => {
         const res = await request("http://localhost:8080").post('/user/profile').set('Cookie', `token=${token}`);
         expect(res.status).toBe(200);
         expect(res.body.message).toBe('User profile found');
-    });    
+    });
+
+    test('Return an error when user is not found', async () => {
+            const req = { email: 'test@example.com' };
+            const res = {
+                status: jest.fn(() => res),
+                json: jest.fn(),
+            };
+
+            Users.findOne = jest.fn().mockResolvedValue(null);
+
+            await UsersAuthController.fetchUserProfile(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(404);
+            expect(res.json).toHaveBeenCalledWith({
+                success: false,
+                message: "User not found"
+            });
+    }); 
 });
