1dd68ff0d441ea1ed6953bfe0d7ec39df70c4d66
Yashu <2021csb1137@iitrpr.ac.in>
2024-03-19 15:26:41 +0530

    template 1 edit is created

--- client/src/App.js
+++ client/src/App.js
@@ -3,37 +3,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./Components/Pages/Login/Login";
 
-import WelcomePage from './Components/Pages/Dashboard/WelcomePage';
-import Profile from './Components/Pages/Profile/Profile';
+import WelcomePage from "./Components/Pages/Dashboard/WelcomePage";
+import Profile from "./Components/Pages/Profile/Profile";
 
 import NavBar from "./Components/Pages/Dashboard/NavBar/NavBar";
 import Register from "./Components/Pages/Register/Register";
 import Verification from "./Components/Pages/Verification/Verification";
 import Homepage from "./Components/Pages/Homepage/Homepage";
 import { TemplateView } from "./Components/Pages/TemplateView/TemplateView";
+import { Template1edit } from "./Components/Pages/Template1edit/Template1edit";
 function App() {
   return (
-
-
-
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<WelcomePage />} />
 
-
-
-
         <Route path="/Signup" element={<Register />} />
         <Route path="/Verification" element={<Verification />} />
         <Route path="/NavBar" element={<NavBar />} />
         <Route path="/Home" element={<Homepage />} />
         <Route path="/templateview" element={<TemplateView />} />
+        <Route path="/template1edit" element={<Template1edit />} />
       </Routes>
     </BrowserRouter>
   );
-
 }
 
 export default App;
