f935609eabec1776630bdc6222a770e9da76debd
karthik-NvY <karthikndasaraju@gmail.com>
2024-03-25 00:50:46 +0530

    added in routes/api

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -6,11 +6,13 @@ const router = new express.Router();
 
 const Ngohandler  = require("../controllers/NgoController");
 const tokenMiddle = require("../middleware/tokenMiddle");
+const taskAutomation = require("../utils/taskAutomation");
 
 
 router.post("/ngoInfo", Ngohandler.fetchNgoInfo); //Route /api/ngoinfo
 router.post("/ngoTask", Ngohandler.FetchNgoTasks); // Route /api/ngotask
 
 router.post("/ngoInfo", tokenMiddle, Ngohandler.fetchNgoInfo); //Route /user/ngoinfopage
+router.post("/taskauto", tokenMiddle, )
 
 module.exports = router;
