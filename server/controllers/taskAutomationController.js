9098034b4a7503f63c91a3de71d6c8a3e739ecd9
karthik-NvY <karthikndasaraju@gmail.com>
2024-03-25 00:57:48 +0530

    adding file structure with controller

---
+++ server/controllers/taskAutomationController.js
@@ -0,0 +1,8 @@
+const taskAutomation = require("../utils/taskAutomation");
+
+static taskAutomationController = async(req, res) => {
+	const { ngoID } = req.body
+	
+};
+
+module.exports = taskAutomationController;
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -6,13 +6,13 @@ const router = new express.Router();
 
 const Ngohandler  = require("../controllers/NgoController");
 const tokenMiddle = require("../middleware/tokenMiddle");
-const taskAutomation = require("../utils/taskAutomation");
+const taskAutomationController = require("../controllers/taskAutomationController");
 
 
 router.post("/ngoInfo", Ngohandler.fetchNgoInfo); //Route /api/ngoinfo
 router.post("/ngoTask", Ngohandler.FetchNgoTasks); // Route /api/ngotask
 
 router.post("/ngoInfo", tokenMiddle, Ngohandler.fetchNgoInfo); //Route /user/ngoinfopage
-router.post("/taskauto", tokenMiddle, )
+router.post("/taskauto", tokenMiddle, taskAutomationController); 
 
 module.exports = router;
