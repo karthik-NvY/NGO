9c653428b5f5ee3d5f1f98f8298288bf03d66d44
karthik-NvY <karthikndasaraju@gmail.com>
2024-04-02 13:11:48 +0530

    added fetching globals

--- server/routes/globalstatusRoute.js
+++ server/routes/globalstatusRoute.js
@@ -3,11 +3,13 @@
 */ 
 
 const express = require('express');
+const tokenMiddle = require("../middleware/tokenMiddle");
 const router = new express.Router();
 
 const GlobalStatushandler = require('../controllers/GlobalStatusController');
 
-router.post("/available", GlobalStatushandler.GlobalAvailable); //Route  /global/available
-router.post("/notavailable", GlobalStatushandler.GlobalNotAvailable); //Route    /global/notavailable
+router.post("/available", tokenMiddle, GlobalStatushandler.GlobalAvailable); //Route  /global/available
+router.post("/notavailable", tokenMiddle, GlobalStatushandler.GlobalNotAvailable); //Route    /global/notavailable
+router.post("/fetchglobals", tokenMiddle, GlobalStatushandler.GlobalFetch) //Route /global/globalfetch
 
 module.exports = router;
