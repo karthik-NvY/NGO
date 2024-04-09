2a4b90f991d0328a7a9eae63f81752dacdbeb014
Akshitha Peguda <2021csb1122@iitrpr.ac.in>
2024-04-09 21:53:48 +0530

    changes

--- server/models/globalAvailModel.js
+++ server/models/globalAvailModel.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const globalAvailSchema = new mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Reference to the user model
-        required: true,
-    },
-    ngo_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'NGO', // Reference to the ngos model
-        required: true,
-    }
-})
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference to the user model
+    required: true,
+  },
+  ngo_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "NGO", // Reference to the ngos model
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('GlobalStatus', globalAvailSchema);
+module.exports = mongoose.model("GlobalStatus", globalAvailSchema);
