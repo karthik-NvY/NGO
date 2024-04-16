a537f79f14bab044250aca467f1a110dc053a8b3
saidatta993 <kolasaidattagoud@gmail.com>
2024-04-16 23:31:06 +0530

    updated test

--- server/__tests__/ExecSignup/ExecSignUp.test.js
+++ server/__tests__/ExecSignup/ExecSignUp.test.js
@@ -1,24 +1,42 @@
 const request = require('supertest')
-
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUGF2aXRocmFuIiwiZW1haWwiOiIyMDIxY3NiMTA4OEBpaXRycHIuYWMuaW4iLCJpZCI6IjY2MDQzODM4MzZhNTJhZjhmNjdlY2MxMCIsImlhdCI6MTcxMzI2Njk2MCwiZXhwIjoxNzEzMzUzMzYwfQ.PlEHLKgoMY8bkaGUAXKEUSGnaithazu-P7TKPVmagQg"
 describe('Fetching Request', () => {
 
     test('Return error if ngo_id is missing', async () => {
-        const packet = {token : token};
+         // signup with new user to get token.
+        const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+        const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+
+        // login with new user to get token.
+        const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token: newloginres.body.token};
         const res = await request("http://localhost:8080").post('/Request/fetch').send(packet)
         expect(res.status).toBe(400);
         expect(res.body.message).toBe('Missing input data');
     });
 
     test('Return if no ngo_id is found in request list', async () => {
-        const packet = {token : token,ngo_id:"GAK986"};
+         // signup with new user to get token.
+        const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+        const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+
+        // login with new user to get token.
+        const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+
+        const packet = {token : newloginres.body.token,ngo_id:"GAK986"};
         const res = await request("http://localhost:8080").post('/Request/fetch').send(packet)
         expect(res.status).toBe(404);
         expect(res.body.message).toBe('No user requested from this ngo');
     });
 
     test('Return if user is successfully fetched ', async () => {
-        const packet = {token : token,ngo_id:"GAK987"};
+         // signup with new user to get token.
+         const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+         const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+ 
+         // login with new user to get token.
+         const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+
+        const packet = {token : newloginres.body.token,ngo_id:"GAK987"};
         const res = await request("http://localhost:8080").post('/Request/fetch').send(packet)
         expect(res.status).toBe(200);
         expect(res.body.message).toBe('Requests fetched successfully');
@@ -28,14 +46,26 @@ describe('Fetching Request', () => {
 describe('storing Request', () => {
 
     test('Return error if ngo_id or user_id is missing', async () => {
-        const packet = {token : token};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token : newloginres.body.token};
         const res = await request("http://localhost:8080").post('/Request/add').send(packet)
         expect(res.status).toBe(400);
         expect(res.body.message).toBe('Missing input data');
     });
 
     test("Return if user's request is successfully added", async () => {
-        const packet = {token : token,ngo_id:"GAK987",  user_id:"6604383836a52af8f67ecc10"};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token :newloginres.body.token,ngo_id:"GAK987",  user_id:"6604383836a52af8f67ecc10"};
         const res = await request("http://localhost:8080").post('/Request/add').send(packet)
         expect(res.status).toBe(201);
         expect(res.body.message).toBe('successfully request added');
@@ -47,21 +77,39 @@ describe('storing Request', () => {
 describe('deleteing Request', () => {
 
     test('Return error if user_id is missing', async () => {
-        const packet = {token : token};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token : newloginres.body.token};
         const res = await request("http://localhost:8080").post('/Request/delete').send(packet)
         expect(res.status).toBe(400);
         expect(res.body.message).toBe('Missing input data');
     });
 
     test('Return if no ngo_id is found in request list', async () => {
-        const packet = {token : token,user_id:"6604383836a52af8f67ecc11"};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token : newloginres.body.token,user_id:"6604383836a52af8f67ecc11"};
         const res = await request("http://localhost:8080").post('/Request/delete').send(packet)
         expect(res.status).toBe(404);
         expect(res.body.message).toBe('No Request found with the provided ID');
     });
 
     test('Return if user is successfully fetched ', async () => {
-        const packet = {token : token,user_id:"6604383836a52af8f67ecc10"};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token : newloginres.body.token,user_id:"6604383836a52af8f67ecc10"};
         const res = await request("http://localhost:8080").post('/Request/delete').send(packet)
         expect(res.status).toBe(200);
         expect(res.body.message).toBe('Request deleted successfully');
@@ -71,21 +119,39 @@ describe('deleteing Request', () => {
 describe('updating Role', () => {
 
     test('Return error if user_id is missing', async () => {
-        const packet = {token : token};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token : newloginres.body.token};
         const res = await request("http://localhost:8080").post('/Request/updateRole').send(packet)
         expect(res.status).toBe(400);
         expect(res.body.message).toBe('Missing input data');
     });
 
     test('Return if no ngo_id is found in request list', async () => {
-        const packet = {token : token,user_id:"6604383836a52af8f67ecc11"};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token : newloginres.body.token,user_id:"6604383836a52af8f67ecc11"};
         const res = await request("http://localhost:8080").post('/Request/updateRole').send(packet)
         expect(res.status).toBe(404);
         expect(res.body.message).toBe('No user found with that id');
     });
 
     test('Return if user is successfully fetched ', async () => {
-        const packet = {token : token,user_id:"DEDVE001009"};
+          // signup with new user to get token.
+          const newuserpacket = {name:"name", password:"pass", email:"tmp@gmail.com"}
+          const newres = await request("http://localhost:8080").post('/user/signup').send(newuserpacket)
+  
+          // login with new user to get token.
+          const newloginres = await request("http://localhost:8080").post('/user/login').send(newuserpacket)
+        const packet = {token : newloginres.body.token,user_id:"DEDVE001009"};
         const res = await request("http://localhost:8080").post('/Request/updateRole').send(packet)
         expect(res.status).toBe(200);
         expect(res.body.message).toBe('role updated successfully');
