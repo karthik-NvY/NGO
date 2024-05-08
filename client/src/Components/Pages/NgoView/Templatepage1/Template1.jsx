18597b19301a0b3319611901947ef484ea871a05
saidatta993 <kolasaidattagoud@gmail.com>
2024-05-08 15:24:10 +0530

    added buttons..

--- client/src/Components/Pages/NgoView/Templatepage1/Template1.jsx
+++ client/src/Components/Pages/NgoView/Templatepage1/Template1.jsx
@@ -1,18 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Template1.css";
 
-import { FaUpload } from "react-icons/fa6";
-import { IoIosCloudUpload } from "react-icons/io";
-import { MdDeleteForever } from "react-icons/md";
-import { MdOutlinePostAdd } from "react-icons/md";
+
 import { FaPhone } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { IoIosMail } from "react-icons/io";
-import { IoMdCloudDone } from "react-icons/io";
-import { MdCloudDone } from "react-icons/md";
-
 import anime from 'animejs/lib/anime.es.js';
+
 
 import axios from "axios";
 
@@ -20,65 +15,26 @@ import setAuthHeaders from "../../../Utils/setAuthHeaders";
 console.log("wughwhiowfwo")
 const Template1 = () => {
   
-  const [upload_icon_anim, setupload_icon_anim] = useState(null)
-  const [upload_text_anim, setupload_text_anim] = useState(null)
 
-  const [aboutus_anim, setaboutus_anim] = useState(null)
-  const [aboutus_heading_anim, setaboutus_heading_anim] = useState(null)
-  const [aboutus_image1_anim, setaboutus_image1_anim] = useState(null)
-  const [aboutus_image2_anim, setaboutus_image2_anim] = useState(null)
-  const [aboutus_text_anim, setaboutus_text_anim] = useState(null)
-  const [publish_text_anim, setpublish_text_anim] = useState(null)
-  const [publish_icon_anim, setpublish_icon_anim] = useState(null)
-  const [publish_button_anim, setpublish_button_anim] = useState(null)
+  const [aboutus_anim1, setaboutus_anim] = useState(null)
+  const [aboutus_heading_anim1, setaboutus_heading_anim] = useState(null)
+  const [aboutus_text_anim1, setaboutus_text_anim] = useState(null)
     const [templateData,setTemplateData]=useState();
     const apiUrl = process.env.REACT_APP_API_URL;
     useEffect(() => {
         fetchTemplateData(); // Fetch user profile data when component mounts
       }, []);
-      useEffect(() => {
-        setupload_icon_anim(anime({
-          targets:document.getElementById('upload-icon-id'),
-          duration: 200,
-          easing: 'easeOutCubic',
-          borderColor: '#000000',
-          autoplay:false,
-        }))
-      
-        setupload_text_anim(anime({
-          targets:document.getElementById('upload-text-id'),
-          width:['0%', '80%'],
-          duration: 200,
-          easing: 'easeOutCubic',
-          autoplay:false,
-        }));
-      
+      useEffect(() => {  
         setaboutus_heading_anim(anime({
-          targets:document.getElementById('about-heading-id'),
+          targets:document.getElementById('about-heading-id1'),
           fontSize:['1%', '300%'],
           duration:2000,
           easing:'easeOutCubic',
           autoplay:false,
         }));
       
-        // setaboutus_image1_anim(anime({
-        //   targets:document.getElementById('about-images-1-id'),
-        //   width:['0%', '100%'],
-        //   duration:2000,
-        //   easing:'easeOutCubic',
-        //   autoplay:false,
-        // }));
-        
-        // setaboutus_image2_anim(anime({
-        //   targets:document.getElementById('about-images-2-id'),
-        //   width:['0%', '100%'],
-        //   duration:2000,
-        //   easing:'easeOutCubic',
-        //   autoplay:false,
-        // }));
-      
         setaboutus_text_anim(anime({
-          targets:document.getElementById('about-text-id'),
+          targets:document.getElementById('about-text-id1'),
           fontSize:['0%', '100%'],
           width:['0%', '100%'],
           duration:2000,
@@ -87,7 +43,7 @@ const Template1 = () => {
         }));
       
         setaboutus_anim(anime({
-          targets:document.getElementById('about-section-id'),
+          targets:document.getElementById('about-section-id1'),
           backgroundImage:['linear-gradient(-90deg, rgba(123, 115, 107, 1) 0px, rgba(214, 207, 200, 1) 100%)',
             'linear-gradient(180deg, rgba(123, 115, 107, 1) 0px, rgba(214, 207, 200, 1) 100%)'],
           duration:4000,
@@ -95,69 +51,20 @@ const Template1 = () => {
           loop:true,
           autoplay:false
         }));
-      
-        setpublish_icon_anim(anime({
-          targets:document.getElementById('publish-icon-id'),
-          duration: 200,
-          width:['100%', '30%'],
-          borderLeft:['0px solid transparent', '2px solid white'],
-          easing: 'easeOutCubic',
-          autoplay:false,
-        }))
-      
-        setpublish_button_anim(anime({
-          targets:document.getElementById('publish-button-id'),
-          width:['4%', '14%'],
-          borderRadius:['10%', '2%'],
-          duration: 200,
-          easing: 'easeOutCubic',
-          autoplay:false,
-        }));
-      
-        setpublish_text_anim(anime({
-          targets:document.getElementById('publish-text-id'),
-          width:['0%', '70%'],
-          duration: 200,
-          easing: 'easeOutCubic',
-          autoplay:false,
-        }));
       },[]);
       
       
       useEffect(() => {
-          if (aboutus_heading_anim) {
+          if (aboutus_heading_anim1) {
             window.addEventListener('scroll', ()=>{
               var scrollstat = (window.scrollY / window.innerHeight);
-              aboutus_heading_anim.seek(scrollstat * aboutus_heading_anim.duration);
+              aboutus_heading_anim1.seek(scrollstat * aboutus_heading_anim1.duration);
           });   
         }
-      }, [aboutus_heading_anim]);
-      
-      // useEffect(() => {
-      //     if (aboutus_image1_anim) {
-      //       window.addEventListener('scroll', ()=>{
-      //         var scrollstat = (window.scrollY / window.innerHeight);
-      //         aboutus_image1_anim.seek(scrollstat * aboutus_image1_anim.duration);
-      //     });   
-      //   }
-      // }, [aboutus_image1_anim]);
-      
-      // useEffect(() => {
-      //     if (aboutus_image2_anim) {
-      //       window.addEventListener('scroll', ()=>{
-      //         if (window.scrollY < window.innerHeight){
-      //           var scrollstat = (window.scrollY / window.innerHeight);  
-      //         }
-      //         else{
-      //           var scrollstat = (2*window.innerHeight - window.scrollY)/window.innerHeight  
-      //         }
-      //         aboutus_image2_anim.seek(scrollstat * aboutus_image2_anim.duration);
-      //     });   
-      //   }
-      // }, [aboutus_image2_anim]);
+      }, [aboutus_heading_anim1]);
       
       useEffect(() => {
-          if (aboutus_text_anim) {
+          if (aboutus_text_anim1) {
             window.addEventListener('scroll', ()=>{
               if (window.scrollY < window.innerHeight){
                 var scrollstat = (window.scrollY / window.innerHeight);  
@@ -165,20 +72,20 @@ const Template1 = () => {
               else{
                 var scrollstat = (2*window.innerHeight - window.scrollY)/window.innerHeight  
               }
-              aboutus_text_anim.seek(scrollstat * aboutus_text_anim.duration);
+              aboutus_text_anim1.seek(scrollstat * aboutus_text_anim1.duration);
           });   
         }
-      }, [aboutus_text_anim]);
+      }, [aboutus_text_anim1]);
       
       useEffect(() => {
-          if (aboutus_anim) {
+          if (aboutus_anim1) {
             window.addEventListener('scroll', ()=>{
               // var scrollstat = (window.scrollY-window.innerHeight)/window.innerHeight;
               var scrollstat = (window.scrollY / window.innerHeight);
-              aboutus_anim.seek(scrollstat * aboutus_anim.duration);
+              aboutus_anim1.seek(scrollstat * aboutus_anim1.duration);
           });   
         }
-      }, [aboutus_anim]);
+      }, [aboutus_anim1]);
       
       const fetchTemplateData = async () => {
         try {
@@ -243,13 +150,15 @@ const Template1 = () => {
             {templateData.visionText}
           </div>
         </div>
-        <section className="about-section1" id="about-section-id">
+        <section className="about-section1" id="about-section-id1">
         <div className="about-content1">
-          <div className="about-heading1" id="about-heading-id">
-            <h2>ABOUT US</h2>
+          <div className="about-heading1" id="about-heading-id1">
+            <div id="about-heading-id1">
+                <h2 >ABOUT US</h2>
+            </div>
           </div>
-          <div className="about-text1" id="about-textarea-id">
-            <div className="about-textarea1">
+          <div className="about-text1" id="about-textarea-id1">
+            <div className="about-textarea1"  id="about-text-id1">
                    {templateData.aboutUsText}
             </div>
           </div>
@@ -302,26 +211,25 @@ const Template1 = () => {
               <h2>Connect With Us</h2>
           </div>
           <div className="contact-details1">
-            <div className="contact-details-email1">
+           {templateData.email && <div className="contact-details-email1">
               <span className="contact-details-email-icon1"><IoIosMail /></span>
               {templateData.email}
-            </div>
-            <div className="contact-details-phone1">
+            </div>}
+           {templateData.phoneNumber && <div className="contact-details-phone1">
               <span className="contact-details-phone-icon1"><FaPhone /></span>
              {templateData.phoneNumber}
-            </div>            
-            <div className="contact-details-x1">
+            </div>}            
+            {templateData.xhandle && <div className="contact-details-x1">
               <span className="contact-details-x-icon1"><FaXTwitter /></span>
               {templateData.xhandle}
-            </div>
-            <div className="contact-details-insta1">
+            </div>}
+            {templateData.instahandle && <div className="contact-details-insta1">
               <span className="contact-details-insta-icon1"><FaInstagram /></span>
               {templateData.instahandle}
-            </div>
+            </div>}
           </div>
           <div className="contact-buttons1">
-            <button className="volunteer-button1">Volunteer</button>
-            <button className="donor-button1">Donor</button>
+            <button className="volunteer-button1">Want to contribute ?</button>
           </div>
         </div>
       </footer>
