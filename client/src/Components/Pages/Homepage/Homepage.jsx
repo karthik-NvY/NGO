f77e058b7a3ac913c84d1e0f0fc8c022482d2dfc
Darakshinda <2021csb1130@iitrpr.ac.in>
2024-05-10 01:20:45 +0530

    ui/ux changes

--- client/src/Components/Pages/Homepage/Homepage.jsx
+++ client/src/Components/Pages/Homepage/Homepage.jsx
@@ -15,46 +15,66 @@ const Homepage = () => {
     const fetchNgos = async () => {
       try {
         const token = localStorage.getItem("token");
-        setAuthHeaders(token); 
-        const response = await axios.post(`${apiUrl}/api/ngoInfo`, { token }, { withCredentials: true });
-        
-        if(response.status === 200 && response.data.message==="NGOs found"){
+        setAuthHeaders(token);
+        const response = await axios.post(
+          `${apiUrl}/api/ngoInfo`,
+          { token },
+          { withCredentials: true }
+        );
+
+        if (response.status === 200 && response.data.message === "NGOs found") {
           setNgos(response.data.allNgos.reverse());
           console.log(response.data);
         }
-      } 
-      catch (error) 
-      {
-        if(error.response.status === 500 && error.response.data.message==="Internal server error while fetching Info of NGOs"){
-          alert('internal server error');
+      } catch (error) {
+        if (
+          error.response.status === 500 &&
+          error.response.data.message ===
+            "Internal server error while fetching Info of NGOs"
+        ) {
+          alert("internal server error");
         }
-        if(error.response.status === 404 && error.response.data.message==="No NGOs found"){
-          alert('No Ngos found');
+        if (
+          error.response.status === 404 &&
+          error.response.data.message === "No NGOs found"
+        ) {
+          alert("No Ngos found");
         }
         console.error("Error fetching NGOs:", error);
       }
 
       try {
         const token = localStorage.getItem("token");
-        setAuthHeaders(token); 
-        const response = await axios.post(`${apiUrl}/templates/ngoBack`, { token }, { withCredentials: true });
-        
-        if(response.status === 200 && response.data.message==="Successfully fetched NGO background"){
+        setAuthHeaders(token);
+        const response = await axios.post(
+          `${apiUrl}/templates/ngoBack`,
+          { token },
+          { withCredentials: true }
+        );
+
+        if (
+          response.status === 200 &&
+          response.data.message === "Successfully fetched NGO background"
+        ) {
           setNgoback(response.data.ngo);
           console.log("data : ", response.data);
         }
-      } 
-      catch (error) 
-      {
-        if(error.response.status === 500 && error.response.data.message==="Internal server error while fetching Info of NGOs"){
-          alert('internal server error');
+      } catch (error) {
+        if (
+          error.response.status === 500 &&
+          error.response.data.message ===
+            "Internal server error while fetching Info of NGOs"
+        ) {
+          alert("internal server error");
         }
-        if(error.response.status === 404 && error.response.data.message==="No NGOs found"){
-          alert('No Ngos found');
+        if (
+          error.response.status === 404 &&
+          error.response.data.message === "No NGOs found"
+        ) {
+          alert("No Ngos found");
         }
         console.error("Error fetching NGOs:", error);
       }
-
     };
 
     fetchNgos();
@@ -66,13 +86,13 @@ const Homepage = () => {
 
   //const ngo_id = localStorage.getItem("ngo_id");
   function findMainByNgoId(targetNgoId) {
-    if(targetNgoId==null){
-      return null
+    if (targetNgoId == null) {
+      return null;
     }
     for (const item of ngoback) {
-        if (item.ngo_id.toString() === targetNgoId.toString()) {
-            return item.main;
-        }
+      if (item.ngo_id.toString() === targetNgoId.toString()) {
+        return item.main;
+      }
     }
     // Return null if the ngo_id is not found
     return null;
@@ -81,18 +101,27 @@ const Homepage = () => {
     <div className="home">
       <NavBar />
       <div className="popular">
-          <div className="head">Popular NGO</div>
-          <div className="websites">
-            {Array.isArray(ngos) &&
-              ngos.map((ngo) => (
-                <a href={`/ngo/${ngo.name}/${ngo._id}`} key={ngo._id} onClick={() => handleViewClick(ngo._id)} className="ngo-link">
-                  <div className="ngo">
-                    <Ngo name={ngo.name} creator={ngo.admin} back={findMainByNgoId(ngo.ngo_id)}/>
-                  </div>
-                </a>
-              ))}
-          </div>
+        <div className="head">NGO Websites</div>
+        <div className="websites">
+          {Array.isArray(ngos) &&
+            ngos.map((ngo) => (
+              <a
+                href={`/ngo/${ngo.name}/${ngo._id}`}
+                key={ngo._id}
+                onClick={() => handleViewClick(ngo._id)}
+                className="ngo-link"
+              >
+                <div className="ngo">
+                  <Ngo
+                    name={ngo.name}
+                    creator={ngo.admin}
+                    back={findMainByNgoId(ngo.ngo_id)}
+                  />
+                </div>
+              </a>
+            ))}
         </div>
+      </div>
       <div className="build">
         <div className="markngo">
           <p>Want to mark your NGO ?</p>
